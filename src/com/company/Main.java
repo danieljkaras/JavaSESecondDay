25a0e7fdc3d068e12dbf66748c3668ebd0c6eede
Daniel Karaś <daniel.j.karas@gmail.com>
2018-01-25 13:38:53 +0100

    Here is some examples of loops, Strings etc.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+        CheckingClass somethingNew = new CheckingClass(8, false);
+        FibonnaciSeries myFiboSeries = new FibonnaciSeries();
+
+        EqualsExample myEqual = new EqualsExample();
+
+        myEqual.setTextA("joanna");
+        myEqual.setTextB("joanna");
+        System.out.println(myEqual.getTextA().hashCode());
+        System.out.println(myEqual.getTextB().hashCode());
+
+
+
+
+        //myFiboSeries.fiboSeries(20);
+        //myFiboSeries.fiboSeriesArray(15);
+        myEqual.stringCompare();
+
     }
 }
